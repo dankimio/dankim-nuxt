17c3e0d5fafc7347f0879ff624546dc34456f17d
Dan Kim <itsdanya@gmail.com>
2021-06-21 02:22:57 +0300

    Add post page and posts directory

---
+++ pages/_slug.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <NuxtLink :to="'/'">
+      <h1>Blog</h1>
+    </NuxtLink>
+
+    <h1>{{ post.title }}</h1>
+    <p>{{ post.description }}</p>
+    <nuxt-content :document="post" />
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content, params }) {
+    const post = await $content('posts', params.slug).fetch()
+
+    return {
+      post
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,34 @@
 <template>
   <div class="container">
     <div>
-      <h1>
-        dankim-nuxt
-      </h1>
+      <NuxtLink :to="'/'">
+        <h1>Blog</h1>
+      </NuxtLink>
+
+      <ul>
+        <li v-for="post of posts" :key="post.slug">
+          <NuxtLink :to="`/${post.slug}`">
+            <h2>{{ post.title }}</h2>
+          </NuxtLink>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData ({ $content, params }) {
+    const posts = await $content('posts')
+      .only(['title', 'description', 'slug'])
+      .sortBy('createdAt', 'asc')
+      .fetch()
+
+    return {
+      posts
+    }
+  }
+}
 </script>
 
 <style>
