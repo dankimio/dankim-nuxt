aa8c301c1e292db30c3f1366cec9943fc7989a39
Dan Kim <itsdanya@gmail.com>
2021-06-21 03:38:13 +0300

    Add links

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,38 @@
 <template>
   <div class="container">
-    <div>
+    <nav>
       <NuxtLink :to="'/'">
-        <h1>Blog</h1>
+        Dan Kim
       </NuxtLink>
 
-      <ul>
-        <li v-for="post of posts" :key="post.slug">
-          <NuxtLink :to="`/${post.slug}`">
-            <h2>{{ post.title }}</h2>
-          </NuxtLink>
-        </li>
-      </ul>
-    </div>
+      <NuxtLink :to="'/'">
+        Blog
+      </NuxtLink>
+
+      <NuxtLink :to="'/'">
+        Travel
+      </NuxtLink>
+      <NuxtLink :to="'/'">
+        Travel
+      </NuxtLink>
+    </nav>
+
+    <ul>
+      <li v-for="post of posts" :key="post.slug">
+        <NuxtLink :to="`/${post.slug}`">
+          <h2>{{ post.title }}</h2>
+        </NuxtLink>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  async asyncData ({ $content, params }) {
+  async asyncData ({ $content }) {
     const posts = await $content('posts')
       .only(['title', 'description', 'slug'])
-      .sortBy('createdAt', 'asc')
+      .sortBy('createdAt', 'desc')
       .fetch()
 
     return {
