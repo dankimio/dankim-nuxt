cb5a965e585fdee8003e1ba21036abbebc647f35
Dan Kim <itsdanya@gmail.com>
2021-06-21 20:35:13 +0300

    Add nav to pages/_slug

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="container max-w-3xl">
-    <NuxtLink :to="'/'">
-      <h1>Blog</h1>
-    </NuxtLink>
+  <div class="container">
+    <Nav />
 
-    <h1>{{ post.title }}</h1>
-    <p>{{ post.description }}</p>
-    <nuxt-content :document="post" />
+    <div class="mx-auto max-w-3xl">
+      <NuxtLink :to="'/'">
+        <h1>Blog</h1>
+      </NuxtLink>
+
+      <h1>{{ post.title }}</h1>
+      <p>{{ post.description }}</p>
+      <nuxt-content :document="post" />
+    </div>
   </div>
 </template>
 
